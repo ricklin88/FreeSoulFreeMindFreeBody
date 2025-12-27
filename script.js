c2d5496ac3c2d335cfc8906610f1d4f0999daebb
laimuIsdude <zicheng213@gmail.com>
2025-12-27 13:48:19 -0700

    add JS open,close, dim effect

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-const dropmenu = document.getElementById('dropmenu');
+const quoteTab = document.getElementById('quoteTab');
+const quoteDrawer = document.getElementById('quoteDrawer');
+const quoteClose = document.getElementById('quoteClose');
+const quoteOverlay = document.getElementById('quoteOverlay');
 
-addEventListener
+function openDrawer() {
+    quoteDrawer.classList.add('open');
+    quoteOverlay.classList.add('open');
+}
+
+function closeDrawer() {
+    quoteDrawer.classList.remove('open');
+    quoteOverlay.classList.remove('open');
+}
+
+quoteTab.addEventListener('click', () => {
+    if(!quoteDrawer.classList.contains('open')) {
+        openDrawer();
+    }
+});
+
+quoteClose.addEventListener('click', () => {
+    closeDrawer();
+});
+
+quoteOverlay.addEventListener('click', () => {
+    closeDrawer();
+});
